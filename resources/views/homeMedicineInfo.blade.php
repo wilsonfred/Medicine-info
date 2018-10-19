bcd5f46ccd171cfa018ff38b41604dd2dc98a121
Enrico Hasnawi <ehasnawi@gmail.com>
2018-10-19 10:34:14 +0700

    Homepage of medicineinfo + css
    CSS and PHP design for Home page of medicine info(not complete yet)

---
+++ resources/views/homeMedicineInfo.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="{{Asset('css/Home.css')}}" rel="stylesheet"/>
+	<title>MedicineInfo</title>
+</head>
+
+
+<header>
+  <div class="navigation-bar">
+      <div id="navigation-container" class="cf">
+        <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Medicine Info</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Comparison</a></li>
+        </ul>
+      </div> 
+    </div>
+</header>
+
+<body>
+
+	<div id="homebackground" class="text-center width100">
+	<img src="{{Asset('image/unnamed.png')}}" alt="MedicineInfo Logo">
+	<div class="background-custom">
+				<h1 id="hello">Medicine-Info</h1>
+				<h3>All Info You Need About Medicine</h3>
+				<a href="#description" class="showmore">About Us!</a>
+	</div>
+	<br></br>
+	<br></br>
+	</div>
+	<div id="homebackground1" class="text-center width100">
+	<h1>Why Us?</h1>
+	
+		<div >
+		<table class="center">
+		<tr>
+			<th><img class="resize-image" src="{{Asset('image/files.png')}}""></th>
+			<th><img class="resize-image" src="{{Asset('image/unnamed.png')}}""></th>
+			<th><img class="resize-image" src="{{Asset('image/compare.png')}}""></th>
+		</tr>
+		<br></br>
+		<tr>
+			<td>All the Information You Need</td>
+			<td>Easy to access</td>
+			<td>Comparison Feature</td>
+		</tr>
+		</table>
+		</div>
+		</div>
+		<br></br>
+</body>
+
+<footer>
+	<div class = square>
+	<div class="text-footer">
+		Copyright 2018 Kelompok WebProg
+	</div>
+	</div>
+</footer>
+</html>
